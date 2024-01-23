3c6c343d33012cf666d3b79898adfe231f41bf50
SB2\jordan <jordan@krisis.ca>
2024-01-23 15:57:15 -0700

    INIT: firebase

--- apps/t3-training/src/environments/environment.ts
+++ apps/t3-training/src/environments/environment.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyAhb2aXuHwhDKWPg2Qc0G_nVJpEVt2YdyU",
+    authDomain: "t3-training-48794.firebaseapp.com",
+    projectId: "t3-training-48794",
+    storageBucket: "t3-training-48794.appspot.com",
+    messagingSenderId: "170427355128",
+    appId: "1:170427355128:web:1145047c4a8500d940d00d",
+  },
+  production: true,
+  useEmulators: false,
+};
